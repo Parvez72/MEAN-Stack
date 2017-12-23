29181a67df003ba2aa1bfdabd4b6bc4fa0ad2684
Parvez <parvezuddin72@gmail.com>
2017-12-23 17:52:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongojs=require('mongojs');
 var db=mongojs('first',['contactList']);
